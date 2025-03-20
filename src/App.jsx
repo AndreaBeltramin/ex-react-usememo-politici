9f38ef6294f476bbf51ae29e97fdf8dfed495a0b
AndreaBeltramin <andreabeltra99@gmail.com>
2025-03-21 00:04:33 +0100

    milestone 2 senza useMemo

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 import "./App.css";
 
 function App() {
 	const [politici, setPolitici] = useState([]);
+	const [search, setSearch] = useState("");
 
 	useEffect(() => {
 		fetch("https://boolean-spec-frontend.vercel.app/freetestapi/politicians")
@@ -11,16 +12,30 @@ function App() {
 			.catch((error) => console.error(error));
 	}, []);
 
+	const politiciFiltrati = politici.filter((politico) => {
+		const isInName = politico.name.toLowerCase().includes(search.toLowerCase());
+		const isInBio = politico.biography
+			.toLowerCase()
+			.includes(search.toLocaleLowerCase);
+		return isInName || isInBio;
+	});
+
 	return (
 		<>
 			<div className="container">
 				<h1 className="ms-5 mt-5 text-center">LISTA POLITICI</h1>
+				<input
+					type="text"
+					placeholder="Cerca un politico"
+					value={search}
+					onChange={(e) => setSearch(e.target.value)}
+				/>
 				<div>
-					{politici.map((politico) => (
-						<div className="card my-2 ">
+					{politiciFiltrati.map((politico) => (
+						<div key={politico.id} className="card my-2 ">
 							<img
 								src={politico.image}
-								className="card-img-top w-25 m-auto mt-2"
+								className="card-img-top w-25 mt-2 ms-4"
 								alt="politico"
 							/>
 							<div className="card-body">
