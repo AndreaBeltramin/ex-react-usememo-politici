923e78d11557b1d249ed0d186a90e8819e0e7474
AndreaBeltramin <andreabeltra99@gmail.com>
2025-03-20 23:32:55 +0100

    fix lista

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,35 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-	async function chiamataPolitici() {
-		const response = await fetch(
-			"https://boolean-spec-frontend.vercel.app/freetestapi/politicians"
-		);
-		const politici = await response.json();
-		console.log(politici);
-		return politici;
-	}
-
-	chiamataPolitici();
 	const [politici, setPolitici] = useState([]);
+
+	useEffect(() => {
+		fetch("https://boolean-spec-frontend.vercel.app/freetestapi/politicians")
+			.then((res) => res.json())
+			.then((data) => setPolitici(data))
+			.catch((error) => console.error(error));
+	}, []);
 
 	return (
 		<>
-			<div>
-				<h1 className="ms-5 mt-5">TITOLO</h1>
-				<p className="ms-5 mt-5">paragrafo</p>
+			<div className="container">
+				<h1 className="ms-5 mt-5 text-center">LISTA POLITICI</h1>
+				<div>
+					{politici.map((politico) => (
+						<div className="card my-2 ">
+							<img
+								src={politico.image}
+								className="card-img-top w-25 m-auto mt-2"
+								alt="politico"
+							/>
+							<div className="card-body">
+								<h5 className="card-title">{politico.name}</h5>
+								<p className="card-text">{politico.biography}</p>
+							</div>
+						</div>
+					))}
+				</div>
 			</div>
 		</>
 	);
